058646061e4130f188bf9527291f38336c3d791a
tranngocbach11 <tranngocbach0111@gmail.com>
2023-10-24 16:52:31 +0700

    Fix model and view of business module

--- business/models/purchase_plus.py
+++ business/models/purchase_plus.py
@@ -5,4 +5,6 @@ class PurchasePlus(models.Model):
     _inherit = "purchase.order"
     _description = "Extend the purchase model"
 
-    purchase_name = fields.Char("Purchase Name")
+    auto_generated = fields.Boolean(string='Auto Generated Purchase Order', copy=False)
+    auto_sale_order_id = fields.Many2one('sale.order', string='Source Sales Order', readonly=True, copy=False)
+
