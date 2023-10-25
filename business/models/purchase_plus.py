f244f35938b743845d9b17a6b73e6d88efd955d4
tranngocbach11 <tranngocbach0111@gmail.com>
2023-10-25 10:16:16 +0700

    Add function to automation create SO from the current PO

--- business/models/purchase_plus.py
+++ business/models/purchase_plus.py
@@ -5,6 +5,22 @@ class PurchasePlus(models.Model):
     _inherit = "purchase.order"
     _description = "Extend the purchase model"
 
-    auto_generated = fields.Boolean(string='Auto Generated Purchase Order', copy=False)
+    auto_generated = fields.Boolean(string='Auto Generated Purchase Order', copy=False, default=True)
     auto_sale_order_id = fields.Many2one('sale.order', string='Source Sales Order', readonly=True, copy=False)
 
+    @api.model
+    def create(self, vals):
+        res = super(PurchasePlus, self).create(vals)
+
+        sale_order_vals = {
+            'partner_id': res.partner_id.id,
+            'order_line': [(0, 0, {
+                'product_id': line.product_id.id,
+                'name': line.name,
+                'product_uom': line.product_uom.id,
+                'price_unit': line.price_unit,
+            }) for line in res.order_line]
+        }
+        sale_order = self.env['sale.order'].create(sale_order_vals)
+
+        return res
